53e06ae42ad6246030e0ccf2bd4669458e90020c
NurulHelda <nurul.helda.tif20@gmail.com>
2023-03-20 13:32:38 +0700

    change buttom

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,20 +1,20 @@
 <template>
         <NavBar />
 
-<section class="bg-white py-20">
+<section class="bg-white py-6">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <button class="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 hover:bg-gradient-to-r hover:from-purple-500 hover:via-pink-500 hover:to-red-500 text-white font-bold py-2 px-4 rounded-md shadow-md" @click="back">Back</button>
+      <button class="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 hover:bg-gradient-to-r hover:from-purple-500 hover:via-pink-500 hover:to-red-500 text-white  font-bold py-2 px-4 rounded-md shadow-md" @click="back">Back</button>
       <div class="text-center">
-        <h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl">
-         <marquee width="50%"> Welcome to My Blog </marquee>
+        <h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl mt-6">
+         <marquee width="50%">Articles</marquee>
         </h2>
-        <p class="mt-4 text-lg text-gray-500">
-          A place where I share my thoughts and experiences on various topics.
+        <p class="mt-4 text-lg text-gray-500 justify-center">
+          A place where share my thoughts and experiences on various topics.
         </p>
       </div>
     </div>
   </section>
-  <div class="container mx-auto">
+  <div class=" sm:px-20 px-10 container mx-auto">
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       <NuxtLink :to="`/articles/${post.id}`" v-for="post in posts.posts" :key="post.id" class="bg-white rounded-lg shadow-md overflow-hidden">
         <img class="h-48 w-full object-cover object-center" :src="`https://source.unsplash.com/random/400x300/?${post.tags[1]}`" alt="Random image">
